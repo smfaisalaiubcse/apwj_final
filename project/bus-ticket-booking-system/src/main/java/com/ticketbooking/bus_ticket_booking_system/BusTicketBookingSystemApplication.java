02a850d93447ee64eb35d8622769d4ec05c5e79c
S M Faisal <smf48406@gmail.com>
2025-06-25 12:58:16 +0600

    final project started

---
+++ project/bus-ticket-booking-system/src/main/java/com/ticketbooking/bus_ticket_booking_system/BusTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticketbooking.bus_ticket_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ project/bus-ticket-booking-system/src/test/java/com/ticketbooking/bus_ticket_booking_system/BusTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketbooking.bus_ticket_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
