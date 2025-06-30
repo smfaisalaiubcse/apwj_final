c86e976723a5fb6aaf067f44a4315e22a66807bf
S M Faisal <itssmfaisal@gmail.com>
2025-07-01 02:05:16 +0600

    acid property added for bus add operation

--- project/bus-ticket-booking-system/src/main/java/com/ticketbooking/bus_ticket_booking_system/service/BusService.java
+++ project/bus-ticket-booking-system/src/main/java/com/ticketbooking/bus_ticket_booking_system/service/BusService.java
@@ -8,6 +8,7 @@ import com.ticketbooking.bus_ticket_booking_system.repository.BusRepository;
 import com.ticketbooking.bus_ticket_booking_system.repository.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,6 +19,7 @@ public class BusService {
     @Autowired private RouteRepository routeRepo;
     @Autowired private BusRepository busRepo;
 
+    @Transactional
     public void addBus(Bus bus) {
         BusCompany company = bus.getCompany();
 
